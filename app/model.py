37dd1e2f79fab59be95e88bdc3ea84fbd0e12331
new5558 <new17353@gmail.com>
2021-12-16 00:57:54 +0700

    feat: add 10 epoch of model with synthesize data

--- app/model.py
+++ app/model.py
@@ -43,7 +43,7 @@ class Model:
             self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='./model/yolov5s.pt', device='cpu')
         else:
             import tflite_runtime.interpreter as tflite
-            interpreter = tflite.Interpreter("./model/efficientdet-lite-real-augment-10.tflite")
+            interpreter = tflite.Interpreter("./model/efficientdet-lite-real--synthesis-augment-10.tflite")
             my_signature = interpreter.get_signature_runner()
             self.model = my_signature
 
